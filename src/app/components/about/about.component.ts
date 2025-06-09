679fc26ed5a8baa901251182633d7ead4e79ba10
Gabriel Alves Soares <78185694+GitHubdoGabriel@users.noreply.github.com>
2025-06-09 15:54:24 -0300

    Add Carrossel Vídeos

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  carrosselIndex = 0;
+  midias = [
+    { type: 'video', src: 'assets/video/quarto.mp4', alt: 'Perfuração de poço artesiano em andamento' },
+    { type: 'video', src: 'assets/video/welcome.mp4', alt: 'Vídeo de perfuração' },
+    { type: 'video', src: 'assets/video/inicio.mp4', alt: 'Perfuração de poço artesiano em andamento' },
+    { type: 'video', src: 'assets/video/segundo.mp4', alt: 'Perfuração de poço artesiano em andamento' },
+    { type: 'video', src: 'assets/video/terceiro.mp4', alt: 'Perfuração de poço artesiano em andamento' },
+  ];
+
+  anterior() {
+    this.carrosselIndex = (this.carrosselIndex - 1 + this.midias.length) % this.midias.length;
+  }
+  proximo() {
+    this.carrosselIndex = (this.carrosselIndex + 1) % this.midias.length;
+  }
+
 }
